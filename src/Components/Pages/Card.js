f21bce6f9e87b40a8fd32ccbd1f4dbcc4b445ade
zeynep <zeynep@brainboxitsolutions.co.uk>
2023-03-09 20:51:33 +0000

    fix import issue

--- src/Components/Pages/Card.js
+++ src/Components/Pages/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // class Card extends React.Component(props) {
 //     state = {
 //         impath: this.props.d.image
